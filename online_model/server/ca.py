69f972cbe5a7323142be843f0865a5b051294282
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-05-12 18:58:05 -0400

    MNT: Move ca server main method to command line

--- online_model/server/ca.py
+++ online_model/server/ca.py
@@ -6,7 +6,6 @@ import time
 from epics import caget, PV
 import numpy as np
 import random
-from MakeModel import SurrogateModel
 import json
 
 
@@ -161,71 +160,3 @@ class SyncedSimPVServer:
 
     def stop_server(self):
         self.serve_data = False
-
-
-def fix_units(unit_str):
-
-    unit_str = unit_str.strip()
-    if len(unit_str.split(" ")) > 1:
-        unit_str = unit_str.split(" ")[-1]
-    unit_str = unit_str.replace("(", "")
-    unit_str = unit_str.replace(")", "")
-
-    return unit_str
-
-
-if __name__ == "__main__":
-    from online_model.model.surrogate_model import OnlineSurrogateModel
-
-    vmname = "smvm"
-
-    sm = OnlineSurrogateModel()
-
-    # Start with the nice example from Lipi
-    default_inputs = {
-        "maxb(2)": 0.06125866317542922,
-        "phi(1)": 8.351877669807294,
-        "q_total": 0.020414630732101164,
-        "sig_x": 0.4065596830730608,
-    }
-    default_output = sm.run(default_inputs)
-
-    cmd_pvdb = {}
-    for ii, input_name in enumerate(sm.scalar_model.input_names):
-        cmd_pvdb[input_name] = {
-            "type": "float",
-            "prec": 8,
-            "value": default_inputs[input_name],
-            "units": fix_units(sm.scalar_model.input_units[ii]),
-            "range": list(sm.scalar_model.input_ranges[ii]),
-        }
-
-    sim_pvdb = {}
-    for ii, output_name in enumerate(sm.scalar_model.output_names):
-        sim_pvdb[output_name] = {
-            "type": "float",
-            "prec": 8,
-            "value": default_output[output_name],
-            "units": fix_units(sm.scalar_model.output_units[ii]),
-        }
-
-    # sim_pvdb['z:pz']={'type': 'float', 'prec': 8, 'count':len(default_output['z:pz']),'units':'mm:delta','value':list(default_output['z:pz'])}
-    sim_pvdb["x:y"] = {
-        "type": "float",
-        "prec": 8,
-        "count": len(default_output["x:y"]),
-        "units": "mm:mm",
-        "value": list(default_output["x:y"]),
-    }
-
-    pv_def = {"prefix": vmname + ":", "input": cmd_pvdb, "output": sim_pvdb}
-    with open("pvdef.json", "w") as fp:
-        json.dump(pv_def, fp, sort_keys=True, indent=4)
-
-    # Add in noise for fun
-    # sim_pvdb['x_95coremit']['scan']=0.2
-    # noise_params = {'x_95coremit':{'sigma':0.5e-7,'dist':'uniform'}}
-    noise_params = {}
-
-    server = SyncedSimPVServer(vmname, cmd_pvdb, sim_pvdb, noise_params, sm)
-    server.start_server()
