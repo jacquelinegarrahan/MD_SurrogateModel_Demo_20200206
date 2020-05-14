64c5cf944ba63efa17542779345dd7cdea8b1381
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-05-14 17:18:15 -0400

    MNT/DOC: Refactor CA server and add docstrings

--- online_model/server/ca.py
+++ online_model/server/ca.py
@@ -2,160 +2,292 @@ import copy
 import time
 import numpy as np
 import random
+from typing import Dict, Mapping, Union
 
 from epics import caget, PV
 from pcaspy import Driver, SimpleServer
 
+from online_model.model.surrogate_model import OnlineSurrogateModel
+from online_model import ARRAY_PVS, PREFIX
+
 
 class SimDriver(Driver):
-    def __init__(self, input_pv_state, output_pv_state, noise_params=None):
+    """
+    Class that reacts to read an write requests to process variables.
+
+    Attributes
+    ----------
+    input_pv_state: dict
+        Dictionary mapping initial input process variables to values.
+
+    output_pv_state: dict
+        Dictionary mapping initial output process variables to values (np.ndarray in the case of image x:y)
+
+    noise_params:
+        Dictionary mapping noisy process variables to a dictionary containing their distribution and standard deviation.
+
+    """
+
+    def __init__(
+        self,
+        input_pv_state: Dict[str, float],
+        output_pv_state: Mapping[str, Union[float, np.ndarray]],
+        noise_params: Dict[str, dict] = {},
+    ) -> None:
+        """
+        Initialize the driver. Store input state, output state, and noise parameters.
+
+        Parameters
+        ----------
+        input_pv_state: dict
+            Dictionary that maps the input process variables to their inital values
+
+        output_pv_state:
+            Dictionary that maps the output process variables to their inital values
+
+        noise_params: dict
+            Dictionary mapping noisy process variables to a dictionary containing their distribution and standard deviation.
+        """
 
         super(SimDriver, self).__init__()
 
+        # track input state, output state, and noise parameters
         self.input_pv_state = input_pv_state
         self.output_pv_state = output_pv_state
+        self.noise_params = noise_params
 
-        if noise_params:
-            self.noise_params = noise_params
-        else:
-            self.noise_params = {}
+    def read(self, pv: str) -> Union[float, np.ndarray]:
+        """
+        Method used by server when clients read a process variable.
 
-    def read(self, reason):
-        if reason in self.output_pv_state:
-            value = self.get_noisy_pv(reason)
+        Parameters
+        ----------
+        pv: str
+            Process variable name
+
+        Returns
+        -------
+        float/np.ndarray
+            Returns the value of the process variable
+
+        Notes
+        -----
+        In the pcaspy documentation, 'reason' is used instead of pv.
+
+        """
+        if pv in self.output_pv_state:
+            value = self.getParam(pv)
+            if self.noise_params:
+                value += self.get_noise(pv)
         else:
-            value = self.getParam(reason)
+            value = self.getParam(pv)
 
         return value
 
-    def write(self, reason, value):
+    def write(self, pv: str, value: Union[float, np.ndarray]) -> bool:
+        """
+        Method used by server when clients write a process variable.
 
-        if reason in self.output_pv_state:
-            print(reason + " is a read-only pv")
+
+        Parameters
+        ----------
+        pv: str
+            Process variable name
+
+        value: float/np.ndarray
+            Value to assign to the process variable.
+
+        Returns
+        -------
+        bool
+            Returns True if the value is accepted, False if rejected
+
+        Notes
+        -----
+        In the pcaspy documentation, 'reason' is used instead of pv.
+        """
+
+        if pv in self.output_pv_state:
+            print(pv + " is a read-only pv")
             return False
+
         else:
 
-            if reason in self.input_pv_state:
-                self.input_pv_state[reason] = value
-            self.setParam(reason, value)
+            if pv in self.input_pv_state:
+                self.input_pv_state[pv] = value
+
+            self.setParam(pv, value)
             self.updatePVs()
+
             return True
 
-    def set_output_pvs(self, outpvs):
-        post_updates = False
-        for opv in outpvs:
-            if opv in self.output_pv_state:
-                self.output_pv_state[opv] = outpvs[opv]
-                self.setParam(opv, self.get_noisy_pv(opv))
-                post_updates = True
+    def set_output_pvs(
+        self, output_pvs: Mapping[str, Union[float, np.ndarray]]
+    ) -> None:
+        """
+        Set output process variables.
 
-        if post_updates:
-            self.updatePVs()
+        Parameters
+        ----------
+        output_pvs: dict
+            Dictionary that maps ouput process variable name to values
+        """
+        # update output process variable state
+        self.output_pv_state.update(output_pvs)
 
-    # def set_pvs(self, pvs):
+        # update the output process variables that have been changed
+        for pv in output_pvs:
+            value = self.output_pv_state[pv]
 
-    #    post_updates = False
+            # add noise
+            if pv in self.noise_params:
+                value += self.get_noise(pv)
 
-    #    for pv in pvs:
-    #        self.setParam(pv, pvs[pv])
-    #        post_updates = True
+            # set parameter with value
+            self.setParam(pv, value)
 
-    #    if post_updates:
-    #        self.updatePVs()
+    def get_noise(self, pv: str) -> float:
+        """
+        Add noise to a process variable.
 
-    def get_noisy_pv(self, pv):
+        Parameters
+        ----------
+        pv: str
+            Process variable name
 
+        Returns
+        -------
+        float
+            Return noise value to add to model output value
+        """
         noise = 0
+
+        # check pv included in noise pv attribute
         if pv in self.noise_params:
+            dist = self.getParam(pv + ":dist")
+            sigma = self.getParam(pv + ":sigma")
 
-            dist = self.getParam(pv + ":dist")  # self.noise_params[pv]['dist']
-            sigma = self.getParam(pv + ":sigma")  # self.noise_params[pv]['sigma']
-
+            # sample uniform distribution
             if dist == "uniform":
                 full_width = np.sqrt(12) * sigma
                 noise = random.uniform(-full_width / 2.0, full_width / 2.0)
 
+            # sample normal distribution
             elif dist == "normal":
                 noise = random.uniform(0, sigma)
 
-        return self.output_pv_state[pv] + noise
+        return noise
 
 
-class SyncedSimPVServer:
+class CAServer:
+    """
+    Server object for channel access process variables that updates and reads process values in a single thread.
 
-    """Defines basic PV server that continuously syncs the input model to the input (command) EPICS PV values
-    and publishes updated model data to output EPICS PVs.  Assumes fast model execution, as the model executes
-    in the main CAS server thread.  CAS for the input and ouput PVs is handled by the SimDriver object"""
+    Attributes
+    ----------
+    model: online_model.model.surrogate_model.OnlineSurrogateModel
+        OnlineSurrogateModel instance used for getting predictions
 
-    def __init__(self, name, input_pvdb, output_pvdb, model, noise_params=None):
+    pvdb: dict
+        Dictionary that maps the process variable string to type (str), prec (precision), value (float), units (str), range (List[float])
 
-        self.name = name
+    input_pv_state: dict
+        Dictionary that maps the input process variables to their current values
 
+    output_pv_state:
+        Dictionary that maps the output process variables to their current values
+
+    server: pcaspy.driver.SimpleServer
+        Server class that interfaces between the channel access client and the driver. Forwards the read/write requests to the driver
+
+    driver: online_model.server.ca.SimDriver
+        Class that reacts to process variable read/write requests
+
+    """
+
+    def __init__(
+        self,
+        input_pvdb: Dict[str, dict],
+        output_pvdb: Dict[str, dict],
+        noise_params: Dict[str, dict] = {},
+        prefix: str = PREFIX,
+    ) -> None:
+        """
+        Create OnlineSurrogateModel instance and initialize output variables by running with the input process variable state, set up the proces variable database and input/output variable tracking, start the server, create the process variables, and start the driver.
+
+        Parameters
+        ----------
+        in_pvdb: dict
+            Dictionary that maps the input process variable string to type (str), prec (precision), value (float), units (str), range (List[float])
+
+        out_pvdb: dict
+            Dictionary that maps the output process variable string to type (str), prec (precision), value (float), units (str), range (List[float])
+
+        noise_params: dict
+            Dictionary that maps noisy output process variables to their distribution and standard deviation
+
+        """
+
+        # initalize model instance
+        self.model = OnlineSurrogateModel()
+
+        # set up db for initializing process variables
         self.pvdb = {}
-        self.input_pv_state = {}
-        self.output_pv_state = {}
 
-        self.model = model
+        # set up input process variables
+        self.pvdb.update(input_pvdb)
+        self.input_pv_state = {pv: input_pvdb[pv]["value"] for pv in input_pvdb}
 
-        for pv in input_pvdb:
-            self.pvdb[pv] = input_pvdb[pv]
-            self.input_pv_state[pv] = input_pvdb[pv]["value"]
+        # get starting output from the model and set up output process variables
+        self.output_pv_state = self.model.run(self.input_pv_state)
+        self.pvdb.update(output_pvdb)
 
-        starting_output = self.model.run(self.input_pv_state)
+        # for array pv values, add count to the db entry
+        for pv in ARRAY_PVS:
+            self.pvdb[pv]["count"] = len(self.output_pv_state[pv])
 
-        output_pv_state = {}
-        for pv in output_pvdb:
-            self.pvdb[pv] = output_pvdb[pv]
+        # set up noise process variables
+        for pv in noise_params:
+            self.pvdb[pv + ":sigma"] = {
+                "type": "float",
+                "value": noise_params[pv]["sigma"],
+            }
+            self.pvdb[pv + ":dist"] = {
+                "type": "char",
+                "count": 100,
+                "value": noise_params[pv]["dist"],
+            }
 
-            # if an array process variable, add count to the db entry
-            if isinstance(starting_output[pv], (np.ndarray,)):
-                self.pvdb[pv]["count"] = len(starting_output[pv])
-
-            output_pv_state[pv] = starting_output[pv]
-
-        if noise_params:
-            for pv in output_pvdb:
-                if pv in noise_params:
-                    self.pvdb[pv + ":sigma"] = {
-                        "type": "float",
-                        "value": noise_params[pv]["sigma"],
-                    }
-                    self.pvdb[pv + ":dist"] = {
-                        "type": "char",
-                        "count": 100,
-                        "value": noise_params[pv]["dist"],
-                    }
-
-        prefix = self.name + ":"
+        # initialize channel access server
         self.server = SimpleServer()
-        self.server.createPV(prefix, self.pvdb)
 
-        self.driver = SimDriver(self.input_pv_state, output_pv_state, noise_params)
+        # create all process variables using the process variables stored in self.pvdb
+        # with the given prefix
+        self.server.createPV(prefix + ":", self.pvdb)
 
-        self.serve_data = False
+        # set up driver for handing read and write requests to process variables
+        self.driver = SimDriver(self.input_pv_state, self.output_pv_state, noise_params)
 
-    def start_server(self):
-
-        self.serve_data = True
-
+    def start_server(self) -> None:
+        """
+        Start the channel access server and continually update.
+        """
         sim_pv_state = copy.deepcopy(self.input_pv_state)
 
-        # Do initial simulation
+        # Initialize output variables
         print("Initializing sim...")
         output_pv_state = self.model.run(self.input_pv_state)
         self.driver.set_output_pvs(output_pv_state)
-        print("...done.")
+        print("...finished initializing.")
 
-        while self.serve_data:
-            # process CA transactions
+        while True:
+            # process channel access transactions
             self.server.process(0.1)
 
+            # check if the input process variable state has been updated as
+            # an indicator of new input values
             while sim_pv_state != self.input_pv_state:
 
                 sim_pv_state = copy.deepcopy(self.input_pv_state)
                 output_pv_state = self.model.run(self.input_pv_state)
-                print(output_pv_state)
                 self.driver.set_output_pvs(output_pv_state)
-
-    def stop_server(self):
-        self.serve_data = False
