f5b3bf52e597459262ffeeae4b11db80229c36b9
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-05-14 17:31:57 -0400

    MNT: Remove old arguments and correct model import

--- bin/commands/serve.py
+++ bin/commands/serve.py
@@ -14,23 +14,12 @@ def start_server(protocol):
 
     PROTOCOL options are 'ca' and 'pva'
     """
-
-    vmname = "smvm"
-
-    from online_model.util import fix_units
-    from online_model.model.surrogate_model import OnlineSurrogateModel
     from online_model import CMD_PVDB, SIM_PVDB
 
     if protocol == "ca":
-        from online_model.server.ca import SyncedSimPVServer
+        from online_model.server.ca import CAServer
 
-        model = OnlineSurrogateModel()
-
-        noise_params = {}
-
-        server = SyncedSimPVServer(
-            vmname, CMD_PVDB, SIM_PVDB, model, noise_params=noise_params
-        )
+        server = CAServer(CMD_PVDB, SIM_PVDB)
         server.start_server()
 
     elif protocol == "pva":
