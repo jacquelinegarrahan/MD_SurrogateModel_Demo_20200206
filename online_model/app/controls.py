2a9ff0f7669d1fc16b0ef26a74722a1309bad27e
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-05-15 14:35:17 -0400

    WIP: Move items to utility builders and create single view page

--- online_model/app/controls.py
+++ online_model/app/controls.py
@@ -1,36 +1,19 @@
-import numpy as np
 import sys
 import os
 from argparse import ArgumentParser
 
-from epics import caget, caput
-from p4p.client.thread import Context
-
 from bokeh.io import curdoc
-from bokeh.models import ColumnDataSource
-from bokeh.plotting import figure
 from bokeh.layouts import column, row
 
 # fix for bokeh path error, maybe theres a better way to do this
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../..")
 
-from online_model import CMD_PVDB, PREFIX
-from online_model.app import build_slider
+from online_model import CMD_PVDB
+from online_model.app import build_sliders
 
-
-sliders = []
-
-for ii, pv in enumerate(CMD_PVDB):
-    title = pv + " (" + CMD_PVDB[pv]["units"] + ")"
-    pvname = PREFIX + ":" + pv
-    step = (CMD_PVDB[pv]["range"][1] - CMD_PVDB[pv]["range"][0]) / 100.0
-    scale = 1
-
-    slider = build_slider(
-        title, pvname, scale, CMD_PVDB[pv]["range"][0], CMD_PVDB[pv]["range"][1], step
-    )
-    sliders.append(slider)
-
+# build sliders for the command process variable database
+sliders = build_sliders(CMD_PVDB)
 scol = column(sliders, width=350)
+
 curdoc().add_root(row(scol))
 curdoc().title = "Online Surrogate Model Virtual Machine"
