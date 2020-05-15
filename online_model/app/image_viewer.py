ebc279b7d228b8a39f299cdf0c54619c327214e8
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-05-15 11:34:11 -0400

    BUG/MNT: Fix axis indexes for server and collect attributes for pva viewer

--- online_model/app/image_viewer.py
+++ online_model/app/image_viewer.py
@@ -50,23 +50,31 @@ class PVImageMonitor:
         self.units = units
 
     def poll(self):
+
+        image_data = {"image": None}
         if PROTOCOL == "ca":
             # self.pv = PV(pv, auto_monitor=True)
             data = caget(self.pvname)
+            nx = data[0]
+            ny = data[1]
+            ext = data[2:6]
+            dw = ext[1] - ext[0]
+            dh = ext[3] - ext[2]
+            image = data[6:]
+            image[np.where(image <= 0)] = 0  # Set negative to zero
+            image = image.reshape(int(nx), int(ny))
 
         elif PROTOCOL == "pva":
             # context returns np array with WRITEABLE=False
             # copy to manipulate array below
-            data = copy.copy(CONTEXT.get(self.pvname))
+            pv = CONTEXT.get(self.pvname)
+            attrib = pv.attrib
+            dw = attrib["dw"]
+            dh = attrib["dh"]
+            nx, ny = pv.shape
+            image = copy.copy(pv)
 
-        nx = data[0]
-        ny = data[1]
-        ext = data[2:6]
-
-        image = data[6:]
-        image[np.where(image <= 0)] = 0  # Set negative to zero
-        image = image.reshape(int(nx), int(ny))
-        return (image, ext)
+        return {"image": [image], "x": [nx], "y": [ny], "dw": [dw], "dh": [dh]}
 
     def get_units(self):
         return self.units.split(":")
@@ -82,17 +90,8 @@ for opv in SIM_PVDB:
 
 current_pv = list(pvimages.keys())[0]
 
-img, ext = pvimages[current_pv].poll()
-
-source = ColumnDataSource(
-    {
-        "image": [img],
-        "x": [ext[0]],
-        "y": [ext[2]],
-        "dw": [ext[1] - ext[0]],
-        "dh": [ext[3] - ext[2]],
-    }
-)
+image_data = pvimages[current_pv].poll()
+source = ColumnDataSource(image_data)
 
 select = Select(title="Image PV", value=current_pv, options=list(pvimages.keys()))
 
@@ -137,17 +136,9 @@ def update():
     p.xaxis.axis_label = variables[-2] + " (" + units[0] + ")"
     p.yaxis.axis_label = variables[-1] + " (" + units[1] + ")"
 
-    img, ext = pvimages[current_pv].poll()
+    image_data = pvimages[current_pv].poll()
 
-    img_obj.data_source.data.update(
-        {
-            "image": [img],
-            "x": [ext[0]],
-            "y": [ext[2]],
-            "dw": [ext[1] - ext[0]],
-            "dh": [ext[3] - ext[2]],
-        }
-    )
+    img_obj.data_source.data.update(image_data)
 
 
 curdoc().add_root(column(row(select), row(p), width=300))
