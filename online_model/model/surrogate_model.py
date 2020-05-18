1fe9d626aad1345d637fc0279173c1e09d1c89bf
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-05-18 18:37:38 -0400

    WIP: Update demo custom model

--- online_model/model/surrogate_model.py
+++ online_model/model/surrogate_model.py
@@ -10,8 +10,6 @@ import random
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import datasets, layers, models
-
-import tensorflow as tf
 from keras.models import model_from_json
 from sklearn.preprocessing import MinMaxScaler
 import h5py
@@ -160,9 +158,11 @@ class SurrogateModel(ABC):
         # load model in thread safe manner
         self.thread_graph = tf.Graph()
         with self.thread_graph.as_default():
-            self.thread_session = tf.Session()
+            self.thread_session = tf.compat.v1.Session()
             with self.thread_session.as_default():
-                self.model = model_from_json(model_info["JSON"].decode("utf-8"))
+                self.model = tf.keras.models.model_from_json(
+                    model_info["JSON"].decode("utf-8")
+                )
                 self.model.load_weights(self.model_file)
 
     @abstractmethod
@@ -396,196 +396,6 @@ def load_model_info(model_file: str) -> ModelInfo:
     return model_info
 
 
-class MySurrogateModel:
-    """
-Example Usage:
-    Load model and use a dictionary of inputs to evaluate the NN.
-    """
-
-    def __init__(self, model_file=None, stock_image_input=None):
-        # Save init
-        self.model_file = model_file
-        self.stock_image_input = stock_image_input
-        # Run control
-        self.configure()
-
-    def __str__(self):
-        if self.type == "scalar":
-            s = f"""The inputs are: {', '.join(self.input_names)} and the outputs: {', '.join(self.output_names)}"""
-        elif self.type == "image":
-            s = f"""The inputs are: {', '.join(self.input_names)} and the output: {', '.join(self.output_names)}"""
-        elif self.type == "both":
-            s = f"""The inputs are: {', '.join(self.input_names)} and the output: {', '.join(self.output_names)}. Requires image input and output as well"""
-        return s
-
-    def configure(self):
-
-        ## Open the File
-        with h5py.File(self.model_file, "r") as h5:
-            attrs = dict(h5.attrs)
-            print("Loaded Attributes successfully")
-        self.__dict__.update(attrs)
-        self.json_string = self.JSON
-        self.model = tf.keras.models.model_from_json(self.json_string.decode("utf-8"))
-        print("Loaded Architecture successfully")
-        self.model.load_weights(self.model_file)
-        print("Loaded Weights successfully")
-        ## Set basic values needed for input and output scaling
-        self.model_value_max = attrs["upper"]
-        self.model_value_min = attrs["lower"]
-
-        # TEMPORARY PATCH FOR INPUT/OUTPUT REDUNDANT VARS
-        rebuilt_order = copy.copy(self.input_ordering)
-        for i, input_val in enumerate(self.input_ordering):
-            if input_val in REDUNDANT_INPUT_OUTPUT:
-                rebuilt_order[i] = f"in_{input_val}"
-        self.input_ordering = rebuilt_order
-
-        rebuilt_order = copy.copy(self.output_ordering)
-        for i, output_val in enumerate(self.output_ordering):
-            if output_val in REDUNDANT_INPUT_OUTPUT:
-                rebuilt_order[i] = f"out_{output_val}"
-        self.output_ordering = rebuilt_order
-
-        if self.type == "image":
-            self.image_scale = self.output_scales[-1]
-            self.image_offset = self.output_offsets[-1]
-            self.output_scales = self.output_scales[:-1]
-            self.output_offsets = self.output_offsets[:-1]
-        elif self.type == "both":
-            self.image_input_scale = self.input_scales[-1]
-            self.image_output_scale = self.output_scales[-1]
-            self.image_offset = self.output_offsets[-1]
-            self.output_scales = self.output_scales[:-1]
-            self.output_offsets = self.output_offsets[:-1]
-            self.scalar_variables = len(self.input_ordering)
-            self.scalar_outputs = len(self.output_ordering)
-
-    def scale_inputs(self, input_values):
-        data_scaled = self.model_value_min + (
-            (input_values - self.input_offsets[0 : self.scalar_variables])
-            * (self.model_value_max - self.model_value_min)
-            / self.input_scales[0 : self.scalar_variables]
-        )
-        return data_scaled
-
-    def scale_outputs(self, output_values):
-        data_scaled = self.model_value_min + (
-            (output_values - self.output_offsets)
-            * (self.model_value_max - self.model_value_min)
-            / self.output_scales
-        )
-        return data_scaled
-
-    def scale_image(self, image_values, scale):
-        data_scaled = image_values / scale
-        return data_scaled
-
-    def unscale_image(self, image_values, scale):
-        data_scaled = image_values * scale
-        return data_scaled
-
-    def predict(self, input_image, input_values):
-        inputs_scalar_scaled = self.scale_inputs(input_values)
-        inputs_image_scaled = self.scale_image(input_image, self.image_input_scale)
-        predicted_output = self.model.predict(
-            [inputs_image_scaled, inputs_scalar_scaled]
-        )
-        predicted_image_scaled = np.array(predicted_output[0])
-        predicted_scalars_scaled = predicted_output[1]
-        predicted_scalars_unscale = self.unscale_outputs(predicted_scalars_scaled)
-        predicted_extents = predicted_scalars_unscale[
-            :, int(self.scalar_outputs - self.ndim[0]) :
-        ]
-        predicted_image_unscaled = self.unscale_image(
-            predicted_image_scaled.reshape(
-                predicted_image_scaled.shape[0], int(self.bins[0] * self.bins[1])
-            ),
-            self.image_output_scale,
-        )
-        return predicted_image_unscaled, predicted_extents, predicted_scalars_unscale
-
-    def unscale_inputs(self, input_values):
-        data_unscaled = (
-            (input_values - self.model_value_min)
-            * (self.input_scales[0 : self.scalar_variables])
-            / (self.model_value_max - self.model_value_min)
-        ) + self.input_offsets[0 : self.scalar_variables]
-        return data_unscaled
-
-    def unscale_outputs(self, output_values):
-        data_unscaled = (
-            (output_values - self.model_value_min)
-            * (self.output_scales)
-            / (self.model_value_max - self.model_value_min)
-        ) + self.output_offsets
-        return data_unscaled
-
-    def evaluate(self, settings):
-        vec = np.array([settings[key] for key in self.input_ordering])
-        image = np.array([settings["image"]])
-        predicted_image, predicted_extents, predicted_scalars = self.predict(
-            image, [vec]
-        )
-        predicted_output = dict(zip(self.output_ordering, predicted_scalars.T))
-        predicted_image = predicted_image.reshape(
-            (int(self.bins[0]), int(self.bins[1]))
-        )
-        predicted_output["extents"] = predicted_extents
-        predicted_output["image"] = predicted_image
-
-        # TEMPORARY PATCH FOR OUTPUT ORDERING
-        rebuilt_output = {}
-        for item in predicted_output:
-            if item in REDUNDANT_INPUT_OUTPUT:
-                rebuilt_output[f"out_{item}"] = predicted_output[item]
-
-            else:
-                rebuilt_output[item] = predicted_output[item]
-
-        return rebuilt_output
-
-    def use_stock_input_image(self):
-        data = np.load(self.stock_image_input)
-        return data
-
-    def generate_random_input(self):
-        if self.type == "both":
-            values = np.zeros(len(self.input_ordering))
-            for i in range(len(self.input_ordering)):
-                values[i] = random.uniform(
-                    self.input_ranges[i][0], self.input_ranges[i][1]
-                )
-            individual = dict(zip(self.input_ordering, values.T))
-            individual["image"] = self.use_stock_input_image()
-        else:
-            values = np.zeros(len(self.input_ordering))
-            for i in range(len(self.input_ordering)):
-                values[i] = random.uniform(
-                    self.input_ranges[i][0], self.input_ranges[i][1]
-                )
-            individual = dict(zip(self.input_ordering, values.T))
-
-        return individual
-
-    def random_evaluate(self):
-        individual = self.generate_random_input()
-        print("phi", individual["phi(1)"])
-        print("maxb", individual["maxb(2)"])
-        print("rad", individual["laser_radius"])
-        print("charge", individual["total_charge:value"])
-        if self.type == "scalar":
-            random_eval_output = self.evaluate(individual)
-            print("Output Generated")
-        elif self.type == "image":
-            random_eval_output, extent = self.evaluate_image(individual)
-            print("Output Generated")
-        else:
-            random_eval_output = self.evaluate(individual)
-            print("Output Generated")
-        return random_eval_output
-
-
 class OnlineSurrogateModel:
     """
     Class for running the executing both the scalar and image model.
@@ -604,22 +414,18 @@ class OnlineSurrogateModel:
     Understand the preprocessing here
     """
 
-    def __init__(self, model_file: str = MODEL_FILE) -> None:
+    def __init__(self, models) -> None:
         """
         Initialize OnlineSurrogateModel instance using given scalar and image model \\
         files.
 
         Parameters
         ----------
-        scalar_model_file: str
-            File path to scalar model h5, defaults to SCALAR_MODEL_FILE loaded \\
-            online_model.__init___
+        models: list
+            list of model objects
 
-        image_model_file: str
-            File path to image model h5, defaults to YAG_MODEL_FILE loaded in \\
-            online_model.__init__
         """
-        self.mymodel = MySurrogateModel(model_file)
+        self.models = models
 
     def run(self, pv_state: Dict[str, float]) -> Mapping[str, Union[float, np.ndarray]]:
         """
@@ -637,30 +443,38 @@ class OnlineSurrogateModel:
         """
         t1 = time.time()
 
-        pv_state["image"] = DEFAULT_LASER_IMAGE
-        predicted_output = self.mymodel.evaluate(pv_state)
-
         output = {}
-        for scalar in self.mymodel.output_ordering:
-            output[scalar] = predicted_output[scalar][0]
 
-        image_array = np.array(predicted_output["image"]).reshape(
-            1, self.mymodel.bins[0] * self.mymodel.bins[1]
-        )
-        image_extents = list(predicted_output["extents"][0])
-
-        image_values = np.zeros((2 + len(image_extents) + image_array.shape[1],))
-
-        image_values[0] = self.mymodel.bins[0]
-        image_values[1] = self.mymodel.bins[1]
-        image_values[2:6] = image_extents
-        image_values[6:] = image_array
-
-        # format image data for viewing
-        output["x:y"] = image_values
+        for model in self.models:
+            predicted_output = model.predict(pv_state)
+            output.update(predicted_output)
 
         t2 = time.time()
         print("Running model...", end="")
         print("Ellapsed time: " + str(t2 - t1))
 
         return output
+
+
+def apply_temporary_ordering_patch(ordering, prefix):
+    # TEMPORARY PATCH FOR INPUT/OUTPUT REDUNDANT VARS
+    rebuilt_order = copy.copy(ordering)
+    for i, val in enumerate(ordering):
+        if val in REDUNDANT_INPUT_OUTPUT:
+            rebuilt_order[i] = f"{prefix}_{val}"
+
+    breakpoint()
+    return rebuilt_order
+
+
+def apply_temporary_output_patch(output):
+    # TEMPORARY PATCH FOR OUTPUT ORDERING
+    rebuilt_output = {}
+    for item in output:
+        if item in REDUNDANT_INPUT_OUTPUT:
+            rebuilt_output[f"out_{item}"] = output[item]
+
+        else:
+            rebuilt_output[item] = output[item]
+
+    return rebuilt_output
