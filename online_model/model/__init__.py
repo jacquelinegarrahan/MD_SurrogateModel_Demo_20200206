449a504309e50c90d2965ebaca1185ddcc1f2134
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-05-20 13:05:15 -0400

    MNT: Move input/output patches to __init__

---
+++ online_model/model/__init__.py
@@ -0,0 +1,25 @@
+# Some input/output variables have the same name and must be unique.
+# Below are utility functions to fix this:
+
+
+def apply_temporary_ordering_patch(ordering, prefix):
+    # TEMPORARY PATCH FOR INPUT/OUTPUT REDUNDANT VARS
+    rebuilt_order = copy.copy(ordering)
+    for i, val in enumerate(ordering):
+        if val in REDUNDANT_INPUT_OUTPUT:
+            rebuilt_order[i] = f"{prefix}_{val}"
+
+    return rebuilt_order
+
+
+def apply_temporary_output_patch(output):
+    # TEMPORARY PATCH FOR OUTPUT ORDERING
+    rebuilt_output = {}
+    for item in output:
+        if item in REDUNDANT_INPUT_OUTPUT:
+            rebuilt_output[f"out_{item}"] = output[item]
+
+        else:
+            rebuilt_output[item] = output[item]
+
+    return rebuilt_output
