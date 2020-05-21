073feaae7532cef49e090c7f613ea50b3d0bc4d1
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-05-20 20:01:18 -0400

    DOC: Document controller

--- online_model/app/controllers.py
+++ online_model/app/controllers.py
@@ -1,9 +1,27 @@
+from typing import Union
+import numpy as np
 from epics import caget, caput
 from p4p.client.thread import Context
 
 
 class Controller:
-    def __init__(self, protocol):
+    """
+    Controller class used to get and put process variables.
+
+    Attributes
+    ----------
+    protocol: str
+        Protocol to use ("pva", "ca")
+
+    context: p4p.client.thread.Context
+        p4p threaded context instance
+
+    """
+
+    def __init__(self, protocol: str):
+        """
+        Store protocol and initialize context if using PVAccess.
+        """
         self.protocol = protocol
 
         # initalize context for pva
@@ -11,14 +29,40 @@ class Controller:
         if protocol == "pva":
             self.context = Context("pva")
 
-    def get(self, pvname):
+    def get(self, pvname: str):
+        """
+        Get the value of a process variable.
+
+        Parameters
+        ----------
+        pvname: str
+            Name of the process variable
+
+        Returns
+        -------
+        np.ndarray
+            Returns numpy array containing value.
+
+        """
         if self.protocol == "ca":
             return caget(pvname)
 
         elif self.protocol == "pva":
             return self.context.get(pvname)
 
-    def put(self, pvname, value):
+    def put(self, pvname, value: Union[np.ndarray, float]) -> None:
+        """
+        Assign the value of a process variable.
+
+        Parameters
+        ----------
+        pvname: str
+            Name of the process variable
+
+        value
+            Value to put. Either float or numpy array
+
+        """
         if self.protocol == "ca":
             caput(pvname, value)
 
