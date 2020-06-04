814e04b65045532f96c5492bcf2a88080a6965e5
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-06-04 16:56:44 -0400

    MNT: Pass xarray without explicit boolean

--- online_model/app/__init__.py
+++ online_model/app/__init__.py
@@ -42,7 +42,8 @@ def parse_args():
         help="Data file to load variables.",
         required=True,
     )
-    parser.add_argument("--from-xarray", type=bool, default=False)
+    parser.add_argument("--from-xarray", default=False, action="store_true")
+    parser.add_argument("--not-xarray", dest="from-xarray", action="store_false")
     args = parser.parse_args()
 
     protocol = args.protocol[0]
