7f7cb83b367260bd34ccf51ae8619044e1a711cf
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-06-05 14:56:13 -0400

    MNT: Remove all default args from notebooks

--- online_model/__init__.py
+++ online_model/__init__.py
@@ -6,6 +6,3 @@ from online_model.util import fix_units, build_image_pvs
 # set keras backend to tensorflow to prevent theano import errors
 os.environ["KERAS_BACKEND"] = "tensorflow"
 os.environ["EPICS_CA_MAX_ARRAY_BYTES"] = "1000000"
-
-
-ARRAY_PVS = ["x:y", "image"]
